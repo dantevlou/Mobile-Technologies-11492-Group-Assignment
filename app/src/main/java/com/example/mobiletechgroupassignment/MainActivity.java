454815d7cfd0d36bcd526da317df12846c161c11
Danganator <Danganator@gmail.com>
2026-04-22 20:05:30 +1000

    Activity 1 - 22/04/2026 8:05 PM

--- app/src/main/java/com/example/mobiletechgroupassignment/MainActivity.java
+++ app/src/main/java/com/example/mobiletechgroupassignment/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mobiletechgroupassignment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton buttonBarcodeReader = findViewById(R.id.barcodeReaderButton);
+        buttonBarcodeReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BarcodeReader.class);
+//                intent.putExtra("message", "Hello World!");
+                startActivity(intent);
+            }
+        });
+
+        ImageButton buttonContentReader = findViewById(R.id.contentReaderButton);
+        buttonContentReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContentReader.class);
+//                intent.putExtra("message", "Hello World!");
+                startActivity(intent);
+            }
+        });
+
+        ImageButton buttonTextReader = findViewById(R.id.textReaderButton);
+        buttonTextReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TextReader.class);
+//                intent.putExtra("message", "Hello World!");
+                startActivity(intent);
+            }
+        });
+
+        // for the analysis button
+//        Button buttonAnalysedList = findViewById(R.id.analysedListButton);
+//        buttonAnalysedList.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, "the page you want to go to".class);
+// //             intent.putExtra("message", "Hello World!");
+//                startActivity(intent);
+//            }
+//        });
     }
+
 }
